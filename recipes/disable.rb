8f826561910c4a0acc715fb979e91c9fe91fa5c0
gonzalez <edds718@gmail.com>
2015-08-04 11:13:25 -0400

    adding recipes

---
+++ recipes/disable.rb
@@ -0,0 +1,9 @@
+remote_file "/tmp/rightlink.disable.sh"
+  source "https://rightlink.rightscale.com/rll/10.1.4/rightlink.disable.sh"
+  mode 0755
+  action :create
+end
+
+execute "disable rightlink" do
+  command "/tmp/rightlink.disable.sh -k  node['rightlink10']['refresh_token'] -f"
+end
---
+++ recipes/enable.rb
@@ -0,0 +1,10 @@
+remote_file "/tmp/rightlink.enable.sh"
+  source "https://rightlink.rightscale.com/rll/10.1.4/rightlink.enable.sh"
+  action :create
+end
+
+
+bash "enable rightlink" do
+
+
+end
