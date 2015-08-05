d47604239abaa02a76bc44855a46220f38ce49c2
gonzalez <edds718@gmail.com>
2015-08-05 15:41:32 -0400

    typo

--- metadata.rb
+++ metadata.rb
@@ -7,8 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 recipe 'rightlink10::enable', "Enables the RightLink 10 agent"
-recipe 'rightlink10::diable', "Disables the RightLink 10 agent"
-recipe 'rightlink10::upgrade',"Upgrades the RightLink 10 agent" 
+recipe 'rightlink10::disable', "Disables the RightLink 10 agent"
+recipe 'rightlink10::upgrade',"Upgrades the RightLink 10 agent"
 
 attribute 'rightlink10/refresh_token',
   :display_name => 'RightScale Refresh Token',
