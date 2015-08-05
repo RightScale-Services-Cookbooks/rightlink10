526a64a4e59118e79374b450e2102fdf43528f19
gonzalez <edds718@gmail.com>
2015-08-05 11:11:29 -0400

    rubocop cleansing

--- recipes/disable.rb
+++ recipes/disable.rb
@@ -1,9 +1,9 @@
-remote_file "/tmp/rightlink.disable.sh"
-  source "https://rightlink.rightscale.com/rll/10.1.4/rightlink.disable.sh"
+remote_file '/tmp/rightlink.disable.sh' do
+  source 'https://rightlink.rightscale.com/rll/10.1.4/rightlink.disable.sh'
   mode 0755
   action :create
 end
 
-execute "disable rightlink" do
+execute 'disable rightlink' do
   command "/tmp/rightlink.disable.sh -k  node['rightlink10']['refresh_token'] -f"
 end
--- recipes/enable.rb
+++ recipes/enable.rb
@@ -1,10 +1,9 @@
-remote_file "/tmp/rightlink.enable.sh"
-  source "https://rightlink.rightscale.com/rll/10.1.4/rightlink.enable.sh"
+remote_file '/tmp/rightlink.enable.sh' do
+  source 'https://rightlink.rightscale.com/rll/10.1.4/rightlink.enable.sh'
+  mode 0755
   action :create
 end
 
-
-bash "enable rightlink" do
-
-
+execute 'enable rightlink' do
+  command "/tmp/rightlink.enable.sh -k  node['rightlink10']['refresh_token'] -n node['rightlink10']['server_name']  -r node['rightlink10']['servertemplate_href']  -c node['rightlink10']['cloud'] -d node['rightlink10']['deployment_name'] "
 end
