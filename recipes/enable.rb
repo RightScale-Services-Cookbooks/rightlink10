551e26054a61d794e257ccc2182512bcc68ca95d
gonzalez <edds718@gmail.com>
2015-08-05 16:37:15 -0400

    updating syntax

--- recipes/enable.rb
+++ recipes/enable.rb
@@ -5,5 +5,6 @@ remote_file '/tmp/rightlink.enable.sh' do
 end
 
 execute 'enable rightlink' do
-  command "/tmp/rightlink.enable.sh -k  #{node[:rightlink10][:refresh_token]} -n #{node[:rightlink10][:server_name]}  -r #{node[:rightlink10][:servertemplate_href]}  -c #{node[:rightlink10][:cloud]} -d #{node[:rightlink10][:deployment_name]} "
+  command "/tmp/rightlink.enable.sh -k  #{node['rightlink10']['refresh_token']}  -n #{node['rightlink10']['server_name']}  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d #{node['rightlink10']['deployment_name']} "
+
 end
