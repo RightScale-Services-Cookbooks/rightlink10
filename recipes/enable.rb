96a070b04927f4e824ca7b3adfbf006402a62ea7
gonzalez <edds718@gmail.com>
2016-03-01 17:55:37 -0500

    idempotent enable option

--- recipes/enable.rb
+++ recipes/enable.rb
@@ -6,4 +6,5 @@ end
 
 execute 'enable rightlink' do
   command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -k  #{node['rightlink10']['refresh_token']}  -n \"#{node['rightlink10']['server_name']}\"  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
+  not_if {File.exists?('/var/lib/rightscale-identity') }
 end
