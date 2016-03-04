baf336b0dbf41cc11695f1521783bbd0be7660ae
gonzalez <edds718@gmail.com>
2016-03-04 18:10:31 -0500

    fixed name of execute

--- recipes/enable.rb
+++ recipes/enable.rb
@@ -6,7 +6,7 @@ end
 
 
 if node['rightlink10']['server_name'] == 'nil'
-  execute 'enablerightlink' do
+  execute 'enable-rightlink' do
     command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
     not_if {File.exists?('/var/lib/rightscale-identity') }
     action :run
