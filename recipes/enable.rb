56cfbe454d5b88b4025559590dbacdf1198847f3
gonzalez <edds718@gmail.com>
2016-03-04 17:52:47 -0500

    removed ruby block, cleaner implementation

--- recipes/enable.rb
+++ recipes/enable.rb
@@ -4,25 +4,16 @@ remote_file "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh" do
   action :create
 end
 
-ruby_block "RightLink Enable" do
-  block do
-    if node['rightlink10']['server_name'] == 'nil'
-      notifies :run, 'execute[enablerightlink]', :immediately
-    else
-      notifies :run, 'execute[enable-rightlink-with-server-name]', :immediately
-    end
+
+if node['rightlink10']['server_name'] == 'nil'
+  execute 'enablerightlink' do
+    command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
+    not_if {File.exists?('/var/lib/rightscale-identity') }
+    action :run
   end
-  action :run
-end
-
-execute 'enablerightlink' do
-  command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
-  not_if {File.exists?('/var/lib/rightscale-identity') }
-  action :nothing
-end
-
-execute 'enable-rightlink-with-server-name' do
-  command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -n \"#{node['rightlink10']['server_name']}\"  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
-  not_if {File.exists?('/var/lib/rightscale-identity') }
-  action :nothing
+else
+  execute 'enable-rightlink-with-server-name' do
+    command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -n \"#{node['rightlink10']['server_name']}\"  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
+    not_if {File.exists?('/var/lib/rightscale-identity') }
+    action :run
 end
