f5192af519ae52e0eca87f775f7549f479737a5d
gonzalez <edds718@gmail.com>
2016-03-04 17:29:15 -0500

    fixing syntax

--- recipes/enable.rb
+++ recipes/enable.rb
@@ -7,21 +7,21 @@ end
 ruby_block "RightLink Enable" do
   block do
     if node['rightlink10']['server_name'] == 'nil'
-      notifies :run, 'execute[enable-rightlink]', :immediately
+      notifies :run, 'execute[enablerightlink]', :immediately
     else
-      notifies :run, 'execute[enable-rightlinkr-with-server-name]', :immediately
+      notifies :run, 'execute[enable-rightlink-with-server-name]', :immediately
     end
   end
   action :run
 end
 
-execute 'enable-rightlink' do
+execute 'enablerightlink' do
   command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
   not_if {File.exists?('/var/lib/rightscale-identity') }
   action :nothing
 end
 
-execute 'enable-rightlinkr-with-server-name' do
+execute 'enable-rightlink-with-server-name' do
   command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -n \"#{node['rightlink10']['server_name']}\"  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
   not_if {File.exists?('/var/lib/rightscale-identity') }
   action :nothing
