889895a4c2eef113dac87d4689f0256b2ccd8f48
gonzalez <edds718@gmail.com>
2016-03-04 17:57:05 -0500

    removed ruby block, cleaner implementation

--- recipes/enable.rb
+++ recipes/enable.rb
@@ -16,4 +16,5 @@ else
     command "#{Chef::Config[:file_cache_path]}/rightlink.enable.sh -a #{node['rightlink10']['api_endpoint']} -k  #{node['rightlink10']['refresh_token']}  -n \"#{node['rightlink10']['server_name']}\"  -r #{node['rightlink10']['servertemplate_href']}  -c #{node['rightlink10']['cloud']} -d \"#{node['rightlink10']['deployment_name']}\" "
     not_if {File.exists?('/var/lib/rightscale-identity') }
     action :run
+  end
 end
